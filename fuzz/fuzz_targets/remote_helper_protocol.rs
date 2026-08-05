9e97b3103310ad3d948744ffbfd16cd43f89388b
Gabriel Ong Zhe Mian <angryapplegravy@gmail.com>
2026-08-05 10:37:20 +0800

    chore: restore paengi tree after history attachment

--- fuzz/fuzz_targets/remote_helper_protocol.rs
+++
@@ -1,8 +0,0 @@
-#![no_main]
-
-use libfuzzer_sys::fuzz_target;
-use yeokcham_cli::remote_helper_protocol::parse_command;
-
-fuzz_target!(|data: &[u8]| {
-    let _ = parse_command(data);
-});
--- fuzz/fuzz_targets/segment_index.rs
+++
@@ -1,8 +0,0 @@
-#![no_main]
-
-use libfuzzer_sys::fuzz_target;
-use yeokcham_core::SegmentIndex;
-
-fuzz_target!(|data: &[u8]| {
-    let _ = SegmentIndex::decode(data, 4_096, 1 << 20);
-});
